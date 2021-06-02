7d14933442bc60ac22ac09f78daaf0539dbbb30d
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-06-02 12:14:36 +0530

    [APP] Added sercom_i2c_master_i2c_eeprom demo application
    MH3-53542

---
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/port.h
@@ -0,0 +1,45 @@
+/**
+ * \brief Instance header file for PIC32CZ8110CA90208
+ *
+ * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software and any derivatives
+ * exclusively with Microchip products. It is your responsibility to comply with third party license
+ * terms applicable to your use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
+ * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+ * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
+ * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+/* file generated from device description version 2021-05-21T19:08:33Z */
+#ifndef _PIC32CZCA90_PORT_INSTANCE_
+#define _PIC32CZCA90_PORT_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for PORT peripheral ========== */
+#define PORT_AHB_IMPLEMENTED                     _UL_(1)    
+#define PORT_BITS                                _UL_(204)  
+#define PORT_DRVSTR                              _UL_(0)    /* Number of DRVSTR bits (0/1/2) */
+#define PORT_EV_NUM                              _UL_(4)    
+#define PORT_GROUPS                              _UL_(7)    
+#define PORT_HADDR_MSB                           _UL_(25)   /* AHB bus address MSB */
+#define PORT_IOBUS_IMPLEMENTED                   _UL_(0)    
+#define PORT_MSB                                 _UL_(203)  
+#define PORT_ODRAIN                              _UL_(1)    /* ODRAIN supported? */
+#define PORT_SECURE_IMPLEMENTED                  _UL_(0)    /* Secure I/Os supported? */
+#define PORT_SLEWLIM                             _UL_(2)    /* Number of SLEWLIM bits (0/1/2) */
+#define PORT_INSTANCE_ID                         _UL_(18)   /* Instance index for PORT */
+#define PORT_MCLK_ID_AHB                         _UL_(22)   /* Index for PORT AHB clock */
+#define PORT_MCLK_ID_APB                         _UL_(23)   /* Index for PORT APB clock */
+#define PORT_PAC_ID                              _UL_(18)   /* Index for PORT registers write protection */
+
+#endif /* _PIC32CZCA90_PORT_INSTANCE_ */
