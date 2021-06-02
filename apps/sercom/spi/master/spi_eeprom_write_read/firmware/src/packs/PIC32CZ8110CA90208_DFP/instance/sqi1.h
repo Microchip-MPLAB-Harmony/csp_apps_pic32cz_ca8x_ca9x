6c23eb0c9ad6c578489b8b10b46cf033b046c58e
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-06-02 15:20:19 +0530

    [APP] Added sercom_spi_master_spi_eeprom_write_read demo application
    MH3-53544

---
+++ apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/sqi1.h
@@ -0,0 +1,40 @@
+/**
+ * \brief Instance header file for PIC32CZ8110CA90208
+ *
+ * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software and any derivatives
+ * exclusively with Microchip products. It is your responsibility to comply with third party license
+ * terms applicable to your use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
+ * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+ * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
+ * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
+/* file generated from device description version 2021-05-21T19:08:33Z */
+#ifndef _PIC32CZCA90_SQI1_INSTANCE_
+#define _PIC32CZCA90_SQI1_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for SQI1 peripheral ========== */
+#define SQI1_CBUF_DEPTH                          _UL_(3)    
+#define SQI1_GCLK_ID                             _UL_(57)   /* Index of Generic Clock */
+#define SQI1_SFR_ADDRESS                         _UL_(0x4f009000) 
+#define SQI1_TRXBUF_DEPTH                        _UL_(8)    
+#define SQI1_XIP_ADDRESS                         _UL_(0x90) 
+#define SQI1_XIP_CS_MSB                          _UL_(1)    
+#define SQI1_XIP_HSEL                            _UL_(0)    
+#define SQI1_INSTANCE_ID                         _UL_(58)   /* Instance index for SQI1 */
+#define SQI1_MCLK_ID_AHB                         _UL_(67)   /* Index for SQI1 AHB clock */
+#define SQI1_PAC_ID                              _UL_(58)   /* Index for SQI1 registers write protection */
+
+#endif /* _PIC32CZCA90_SQI1_INSTANCE_ */
