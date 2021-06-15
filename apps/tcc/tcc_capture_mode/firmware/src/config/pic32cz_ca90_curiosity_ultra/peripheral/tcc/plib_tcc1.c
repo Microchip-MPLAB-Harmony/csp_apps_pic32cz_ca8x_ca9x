bfead415218f4105ed2d9be71362a7ac821511a8
Arpan Kumar <arpan.kumar@microchip.com>
2021-06-15 23:40:48 +0530

    [TCC] added capture mode demo
    MH3-54701

---
+++ apps/tcc/tcc_capture_mode/firmware/src/config/pic32cz_ca90_curiosity_ultra/peripheral/tcc/plib_tcc1.c
@@ -0,0 +1,200 @@
+/*******************************************************************************
+  Timer/Counter(TCC1) PLIB
+
+  Company
+    Microchip Technology Inc.
+
+  File Name
+    plib_TCC1.c
+
+  Summary
+    TCC1 PLIB Implementation File.
+
+  Description
+    This file defines the interface to the TCC peripheral library. This
+    library provides access to and control of the associated peripheral
+    instance.
+
+  Remarks:
+    None.
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+/* This section lists the other files that are included in this file.
+*/
+#include "interrupts.h"
+#include "plib_tcc1.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data
+// *****************************************************************************
+// *****************************************************************************
+
+static TCC_CALLBACK_OBJECT TCC1_CallbackObject;
+// *****************************************************************************
+// *****************************************************************************
+// Section: TCC1 Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+void TCC1_CaptureInitialize( void )
+{
+    /* Reset TCC */
+    TCC1_REGS->TCC_CTRLA = TCC_CTRLA_SWRST_Msk;
+
+    while((TCC1_REGS->TCC_SYNCBUSY & TCC_SYNCBUSY_SWRST_Msk) == TCC_SYNCBUSY_SWRST_Msk)
+    {
+        /* Wait for Write Synchronization */
+    }
+
+    /* Configure prescaler, standby & capture mode */
+    TCC1_REGS->TCC_CTRLA = TCC_CTRLA_PRESCALER_DIV4 | TCC_CTRLA_PRESCSYNC_PRESC
+                                  | TCC_CTRLA_CPTEN0_Msk | TCC_CTRLA_CPTEN1_Msk
+                                  ;
+
+
+    TCC1_REGS->TCC_EVCTRL = TCC_EVCTRL_TCEI1_Msk | TCC_EVCTRL_EVACT1_PWP | TCC_EVCTRL_MCEI0_Msk | TCC_EVCTRL_MCEI1_Msk;
+
+    /* Clear all interrupt flags */
+    TCC1_REGS->TCC_INTFLAG = TCC_INTFLAG_Msk;
+
+    TCC1_CallbackObject.callback_fn = NULL;
+    TCC1_REGS->TCC_INTENSET = TCC_INTENSET_MC0_Msk | TCC_INTENSET_MC1_Msk;
+    while((TCC1_REGS->TCC_SYNCBUSY) != 0U)
+    {
+        /* Wait for Write Synchronization */
+    }
+}
+
+
+void TCC1_CaptureStart( void )
+{
+    /* Enable TCC */
+    TCC1_REGS->TCC_CTRLA |= TCC_CTRLA_ENABLE_Msk;
+
+    while((TCC1_REGS->TCC_SYNCBUSY & TCC_SYNCBUSY_ENABLE_Msk) == TCC_SYNCBUSY_ENABLE_Msk)
+    {
+        /* Wait for Write Synchronization */
+    }
+}
+
+void TCC1_CaptureStop( void )
+{
+    /* Disable TCC */
+    TCC1_REGS->TCC_CTRLA &= ~TCC_CTRLA_ENABLE_Msk;
+
+    while((TCC1_REGS->TCC_SYNCBUSY & TCC_SYNCBUSY_ENABLE_Msk) == TCC_SYNCBUSY_ENABLE_Msk)
+    {
+        /* Wait for Write Synchronization */
+    }
+}
+
+
+void TCC1_CaptureCommandSet(TCC_COMMAND command)
+{
+    TCC1_REGS->TCC_CTRLBSET = (uint8_t)((uint32_t)command << TCC_CTRLBSET_CMD_Pos);
+    while((TCC1_REGS->TCC_SYNCBUSY) != 0U)
+    {
+        /* Wait for Write Synchronization */
+    }    
+}
+
+
+uint32_t TCC1_Capture32bitValueGet( TCC1_CHANNEL_NUM channel )
+{
+    return (TCC1_REGS->TCC_CC[channel]);
+}
+
+uint32_t TCC1_CaptureFrequencyGet( void )
+{
+    return (uint32_t)(37500000U);
+}
+
+/* Get the current counter value */
+uint32_t TCC1_Capture32bitCounterGet( void )
+{
+    /* Write command to force COUNT register read synchronization */
+    TCC1_REGS->TCC_CTRLBSET |= (uint8_t)TCC_CTRLBSET_CMD_READSYNC;
+
+    while((TCC1_REGS->TCC_SYNCBUSY & TCC_SYNCBUSY_CTRLB_Msk) == TCC_SYNCBUSY_CTRLB_Msk)
+    {
+        /* Wait for Write Synchronization */
+    }
+
+    while((TCC1_REGS->TCC_CTRLBSET & TCC_CTRLBSET_CMD_Msk) != 0U)
+    {
+        /* Wait for CMD to become zero */
+    }
+
+    /* Read current count value */
+    return TCC1_REGS->TCC_COUNT;
+}
+
+/* Register callback function */
+void TCC1_CaptureCallbackRegister( TCC_CALLBACK callback, uintptr_t context )
+{
+    TCC1_CallbackObject.callback_fn = callback;
+
+    TCC1_CallbackObject.context = context;
+}
+
+
+/* Interrupt Handler */
+void TCC1_MC0_InterruptHandler(void)
+{
+    uint32_t status;
+    status = (uint32_t)TCC_INTFLAG_MC0_Msk;
+    /* Clear interrupt flags */
+    TCC1_REGS->TCC_INTFLAG = TCC_INTFLAG_MC0_Msk;
+    if (TCC1_CallbackObject.callback_fn != NULL)
+    {
+        TCC1_CallbackObject.callback_fn(status, TCC1_CallbackObject.context);
+    }
+
+}
+/* Interrupt Handler */
+void TCC1_MC1_InterruptHandler(void)
+{
+    uint32_t status;
+    status = (uint32_t)TCC_INTFLAG_MC1_Msk;
+    /* Clear interrupt flags */
+    TCC1_REGS->TCC_INTFLAG = TCC_INTFLAG_MC1_Msk;
+    if (TCC1_CallbackObject.callback_fn != NULL)
+    {
+        TCC1_CallbackObject.callback_fn(status, TCC1_CallbackObject.context);
+    }
+
+}
+  
+
