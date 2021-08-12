8565c06dd7c7c801208efbbc0b38623a1859c7ee
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-12 10:49:51 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_self_loopback_interrupt_pic32cz_ca90_curiosity_ultra
    MH3-57131
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/master/spi_self_loopback_interrupt/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/sercom1.h
+++ apps/sercom/spi/master/spi_self_loopback_interrupt/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/sercom1.h
@@ -1,5 +1,5 @@
-/**
- * \brief Instance header file for PIC32CZ8110CA90208
+/*
+ * Instance header file for PIC32CZ8110CA90208
  *
  * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
  *
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2021-05-21T19:08:33Z */
+/* file generated from device description version 2021-07-22T19:46:10Z */
 #ifndef _PIC32CZCA90_SERCOM1_INSTANCE_
 #define _PIC32CZCA90_SERCOM1_INSTANCE_
 
@@ -49,5 +49,16 @@
 #define SERCOM1_INSTANCE_ID                      _UL_(24)   /* Instance index for SERCOM1 */
 #define SERCOM1_MCLK_ID_APB                      _UL_(32)   /* Index for SERCOM1 APB clock */
 #define SERCOM1_PAC_ID                           _UL_(24)   /* Index for SERCOM1 registers write protection */
+#define SERCOM1_USART_ERROR_INT_SRC              _UL_(62)   /* USART/SPI/I2C Error */
+#define SERCOM1_USART_TX_READY_INT_SRC           _UL_(64)   /* USART-SPI Data Register Empty/ I2C Stop Received */
+#define SERCOM1_USART_TX_COMPLETE_INT_SRC        _UL_(65)   /* USART-SPI Transmit Complete/ I2C Address Match */
+#define SERCOM1_USART_RX_INT_SRC                 _UL_(66)   /* USART-SPI Receive Complete/ I2C Data Ready */
+#define SERCOM1_SPI_TX_READY_INT_SRC             _UL_(64)   /* USART-SPI Data Register Empty/ I2C Stop Received */
+#define SERCOM1_SPI_TX_COMPLETE_INT_SRC          _UL_(65)   /* USART-SPI Transmit Complete/ I2C Address Match */
+#define SERCOM1_SPI_RX_INT_SRC                   _UL_(66)   /* USART-SPI Receive Complete/ I2C Data Ready */
+#define SERCOM1_I2C_0_INT_SRC                    _UL_(62)   /* USART/SPI/I2C Error */
+#define SERCOM1_I2C_1_INT_SRC                    _UL_(66)   /* USART-SPI Receive Complete/ I2C Data Ready */
+#define SERCOM1_I2C_2_INT_SRC                    _UL_(67)   /* USART Receive Start/ SPI Slave Select Low/ I2C TX FIFO Empty */
+#define SERCOM1_I2C_3_INT_SRC                    _UL_(68)   /* USART Clear To Send Input Change/ I2C RX FIFO Full */
 
 #endif /* _PIC32CZCA90_SERCOM1_INSTANCE_ */
