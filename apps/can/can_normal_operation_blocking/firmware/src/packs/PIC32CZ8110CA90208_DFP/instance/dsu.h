312c3c1fbbfea0fab9992ef679d5252cfbcd4be5
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 01:32:02 +0530

    [CAN] Regenerated demo for can_can_normal_operation_blocking_pic32cz_ca90_curiosity_ultra
    MH3-66073
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/can/can_normal_operation_blocking/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/dsu.h
+++ apps/can/can_normal_operation_blocking/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/dsu.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for PIC32CZ8110CA90208
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,19 +20,19 @@
  *
  */
 
-/* file generated from device description version 2021-07-22T19:46:10Z */
+/* file generated from device description version 2022-03-10T17:15:57Z */
 #ifndef _PIC32CZCA90_DSU_INSTANCE_
 #define _PIC32CZCA90_DSU_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for DSU peripheral ========== */
-#define DSU_BMX_ERROR_SUPPORT                    _UL_(1)    /* Defines whether DSU APB errors are reported as 2-cycles bus errors to the AHB AP0 master */
-#define DSU_DAL1_IMPLEMENTED                     _UL_(0)    /* Debug Access Level 1 (Non-Secure) Implemented? for TZ devices only */
-#define DSU_DMAC_ID_DCC0                         _UL_(2)    /* DMAC ID for DCC0 register */
-#define DSU_DMAC_ID_DCC1                         _UL_(3)    /* DMAC ID for DCC1 register */
-#define DSU_INSTANCE_ID                          _UL_(0)    /* Instance index for DSU */
-#define DSU_MCLK_ID_AHB                          _UL_(0)    /* Index for DSU AHB clock */
-#define DSU_MCLK_ID_APB                          _UL_(1)    /* Index for DSU APB clock */
-#define DSU_PAC_ID                               _UL_(0)    /* Index for DSU registers write protection */
+#define DSU_BMX_ERROR_SUPPORT                    (1)        /* Defines whether DSU APB errors are reported as 2-cycles bus errors to the AHB AP0 master */
+#define DSU_DAL1_IMPLEMENTED                     (0)        /* Debug Access Level 1 (Non-Secure) Implemented? for TZ devices only */
+#define DSU_DMAC_ID_DCC0                         (2)        /* DMAC ID for DCC0 register */
+#define DSU_DMAC_ID_DCC1                         (3)        /* DMAC ID for DCC1 register */
+#define DSU_INSTANCE_ID                          (0)        /* Instance index for DSU */
+#define DSU_MCLK_ID_AHB                          (0)        /* Index for DSU AHB clock */
+#define DSU_MCLK_ID_APB                          (1)        /* Index for DSU APB clock */
+#define DSU_PAC_ID                               (0)        /* Index for DSU registers write protection */
 
 #endif /* _PIC32CZCA90_DSU_INSTANCE_ */
