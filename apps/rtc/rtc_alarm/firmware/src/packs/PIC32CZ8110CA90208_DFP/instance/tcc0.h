98992cbe0b23995e7010e90a2a90a0ddd1ce3e80
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 01:57:01 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_pic32cz_ca90_curiosity_ultra
    MH3-66214
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/tcc0.h
+++ apps/rtc/rtc_alarm/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/tcc0.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for PIC32CZ8110CA90208
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,33 +20,33 @@
  *
  */
 
-/* file generated from device description version 2021-07-22T19:46:10Z */
+/* file generated from device description version 2022-03-10T17:15:57Z */
 #ifndef _PIC32CZCA90_TCC0_INSTANCE_
 #define _PIC32CZCA90_TCC0_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for TCC0 peripheral ========== */
-#define TCC0_CC_NUM                              _UL_(8)    /* Number of Compare/Capture units */
-#define TCC0_DITHERING                           _UL_(1)    /* Dithering feature implemented */
-#define TCC0_DMAC_ID_MC_0                        _UL_(26)   /* Indexes of DMA Match/Compare triggers 0 */
-#define TCC0_DMAC_ID_MC_1                        _UL_(27)   /* Indexes of DMA Match/Compare triggers 1 */
-#define TCC0_DMAC_ID_MC_2                        _UL_(28)   /* Indexes of DMA Match/Compare triggers 2 */
-#define TCC0_DMAC_ID_MC_3                        _UL_(29)   /* Indexes of DMA Match/Compare triggers 3 */
-#define TCC0_DMAC_ID_MC_4                        _UL_(30)   /* Indexes of DMA Match/Compare triggers 4 */
-#define TCC0_DMAC_ID_MC_5                        _UL_(31)   /* Indexes of DMA Match/Compare triggers 5 */
-#define TCC0_DMAC_ID_MC_6                        _UL_(32)   /* Indexes of DMA Match/Compare triggers 6 */
-#define TCC0_DMAC_ID_MC_7                        _UL_(33)   /* Indexes of DMA Match/Compare triggers 7 */
-#define TCC0_DMAC_ID_OVF                         _UL_(25)   /* DMA overflow/underflow/retrigger trigger */
-#define TCC0_DTI                                 _UL_(1)    /* Dead-Time-Insertion feature implemented */
-#define TCC0_GCLK_ID                             _UL_(31)   /* Index of Generic Clock */
-#define TCC0_MASTER_SLAVE_MODE                   _UL_(0)    /* TCC type 0 : NA, 1 : Master, 2 : Slave */
-#define TCC0_OTMX                                _UL_(1)    /* Output Matrix feature implemented */
-#define TCC0_OW_NUM                              _UL_(8)    /* Number of Output Waveforms */
-#define TCC0_PG                                  _UL_(1)    /* Pattern Generation feature implemented */
-#define TCC0_SIZE                                _UL_(32)   
-#define TCC0_SWAP                                _UL_(1)    /* DTI outputs swap feature implemented */
-#define TCC0_INSTANCE_ID                         _UL_(33)   /* Instance index for TCC0 */
-#define TCC0_MCLK_ID_APB                         _UL_(41)   /* Index for TCC0 APB clock */
-#define TCC0_PAC_ID                              _UL_(33)   /* Index for TCC0 registers write protection */
+#define TCC0_CC_NUM                              (8)        /* Number of Compare/Capture units */
+#define TCC0_DITHERING                           (1)        /* Dithering feature implemented */
+#define TCC0_DMAC_ID_MC_0                        (26)       /* Indexes of DMA Match/Compare triggers 0 */
+#define TCC0_DMAC_ID_MC_1                        (27)       /* Indexes of DMA Match/Compare triggers 1 */
+#define TCC0_DMAC_ID_MC_2                        (28)       /* Indexes of DMA Match/Compare triggers 2 */
+#define TCC0_DMAC_ID_MC_3                        (29)       /* Indexes of DMA Match/Compare triggers 3 */
+#define TCC0_DMAC_ID_MC_4                        (30)       /* Indexes of DMA Match/Compare triggers 4 */
+#define TCC0_DMAC_ID_MC_5                        (31)       /* Indexes of DMA Match/Compare triggers 5 */
+#define TCC0_DMAC_ID_MC_6                        (32)       /* Indexes of DMA Match/Compare triggers 6 */
+#define TCC0_DMAC_ID_MC_7                        (33)       /* Indexes of DMA Match/Compare triggers 7 */
+#define TCC0_DMAC_ID_OVF                         (25)       /* DMA overflow/underflow/retrigger trigger */
+#define TCC0_DTI                                 (1)        /* Dead-Time-Insertion feature implemented */
+#define TCC0_GCLK_ID                             (31)       /* Index of Generic Clock */
+#define TCC0_MASTER_SLAVE_MODE                   (0)        /* TCC type 0 : NA, 1 : Master, 2 : Slave */
+#define TCC0_OTMX                                (1)        /* Output Matrix feature implemented */
+#define TCC0_OW_NUM                              (8)        /* Number of Output Waveforms */
+#define TCC0_PG                                  (1)        /* Pattern Generation feature implemented */
+#define TCC0_SIZE                                (32)       
+#define TCC0_SWAP                                (1)        /* DTI outputs swap feature implemented */
+#define TCC0_INSTANCE_ID                         (33)       /* Instance index for TCC0 */
+#define TCC0_MCLK_ID_APB                         (41)       /* Index for TCC0 APB clock */
+#define TCC0_PAC_ID                              (33)       /* Index for TCC0 registers write protection */
 
 #endif /* _PIC32CZCA90_TCC0_INSTANCE_ */
