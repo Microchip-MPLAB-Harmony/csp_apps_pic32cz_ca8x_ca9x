6517ba65968610f63ae5912f8509f3a75169d42e
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-13 01:36:37 +0530

    [DMA] Regenerated demo for dma_dma_memory_transfer_pic32cz_ca90_curiosity_ultra
    MH3-66076
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/dma/dma_memory_transfer/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/adc.h
+++ apps/dma/dma_memory_transfer/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/adc.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for PIC32CZ8110CA90208
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,30 +20,30 @@
  *
  */
 
-/* file generated from device description version 2021-07-22T19:46:10Z */
+/* file generated from device description version 2022-03-10T17:15:57Z */
 #ifndef _PIC32CZCA90_ADC_INSTANCE_
 #define _PIC32CZCA90_ADC_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for ADC peripheral ========== */
-#define ADC_APB_FIFO_DEPTH                       _UL_(16)   /* Number of Words in APB FIFO */
-#define ADC_CHNSAR_NMBR1                         _UL_(16)   /* Number of Channels on SARCORE number 1 */
-#define ADC_CHNSAR_NMBR2                         _UL_(8)    /* Number of Channels on SARCORE number 2 */
-#define ADC_CHNSAR_NMBR3                         _UL_(8)    /* Number of Channels on SARCORE number 3 */
-#define ADC_CHNSAR_NMBR4                         _UL_(7)    /* Number of Channels on SARCORE number 4 */
-#define ADC_DCMP_NMBR                            _UL_(4)    /* Number of Digital Comparators Max=4 */
-#define ADC_DMAC_ID_PFFRDY                       _UL_(75)   
-#define ADC_EARLY_INTR_PRESENT                   _UL_(1)    /* Early Interrupts Present */
-#define ADC_FLTR_NMBR                            _UL_(4)    /* Number of Digital Filters, Max=0 */
-#define ADC_GCLK_ID                              _UL_(41)   /* Index of Generic Clock */
-#define ADC_OCP_ADDR_WIDTH                       _UL_(16)   /* OCP Address Width */
-#define ADC_OCP_FIFO_DEPTH                       _UL_(16)   /* Number of Words in OCP DMA FIFO */
-#define ADC_OCP_PORT_PRESENT                     _UL_(0)    /* OCP DMA Bus Master Present */
-#define ADC_SARCORE_NMBR                         _UL_(4)    /* Number of SARCOREs. Max=4, Min-1 */
-#define ADC_TRGS_NMBR                            _UL_(16)   /* Number of Trigger Sources, Max=16 */
-#define ADC_VREF_STARTUP_TIME                    _UL_(50)   /* Number of Control Clock cycles for VREF to stabalize */
-#define ADC_INSTANCE_ID                          _UL_(43)   /* Instance index for ADC */
-#define ADC_MCLK_ID_APB                          _UL_(51)   /* Index for ADC APB clock */
-#define ADC_PAC_ID                               _UL_(43)   /* Index for ADC registers write protection */
+#define ADC_APB_FIFO_DEPTH                       (16)       /* Number of Words in APB FIFO */
+#define ADC_CHNSAR_NMBR1                         (16)       /* Number of Channels on SARCORE number 1 */
+#define ADC_CHNSAR_NMBR2                         (8)        /* Number of Channels on SARCORE number 2 */
+#define ADC_CHNSAR_NMBR3                         (8)        /* Number of Channels on SARCORE number 3 */
+#define ADC_CHNSAR_NMBR4                         (7)        /* Number of Channels on SARCORE number 4 */
+#define ADC_DCMP_NMBR                            (4)        /* Number of Digital Comparators Max=4 */
+#define ADC_DMAC_ID_PFFRDY                       (75)       
+#define ADC_EARLY_INTR_PRESENT                   (1)        /* Early Interrupts Present */
+#define ADC_FLTR_NMBR                            (4)        /* Number of Digital Filters, Max=0 */
+#define ADC_GCLK_ID                              (41)       /* Index of Generic Clock */
+#define ADC_OCP_ADDR_WIDTH                       (16)       /* OCP Address Width */
+#define ADC_OCP_FIFO_DEPTH                       (16)       /* Number of Words in OCP DMA FIFO */
+#define ADC_OCP_PORT_PRESENT                     (0)        /* OCP DMA Bus Master Present */
+#define ADC_SARCORE_NMBR                         (4)        /* Number of SARCOREs. Max=4, Min-1 */
+#define ADC_TRGS_NMBR                            (16)       /* Number of Trigger Sources, Max=16 */
+#define ADC_VREF_STARTUP_TIME                    (50)       /* Number of Control Clock cycles for VREF to stabalize */
+#define ADC_INSTANCE_ID                          (43)       /* Instance index for ADC */
+#define ADC_MCLK_ID_APB                          (51)       /* Index for ADC APB clock */
+#define ADC_PAC_ID                               (43)       /* Index for ADC registers write protection */
 
 #endif /* _PIC32CZCA90_ADC_INSTANCE_ */
--- apps/dma/dma_memory_transfer/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/can4.h
+++ apps/dma/dma_memory_transfer/firmware/src/packs/PIC32CZ8110CA90208_DFP/instance/can4.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for PIC32CZ8110CA90208
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,20 +20,20 @@
  *
  */
 
-/* file generated from device description version 2021-07-22T19:46:10Z */
+/* file generated from device description version 2022-03-10T17:15:57Z */
 #ifndef _PIC32CZCA90_CAN4_INSTANCE_
 #define _PIC32CZCA90_CAN4_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for CAN4 peripheral ========== */
-#define CAN4_DMAC_ID_DEBUG                       _UL_(87)   /* DMA CAN Debug Req */
-#define CAN4_ECC_BERR_IMPLEMENTED                _UL_(0)    /* ECC error */
-#define CAN4_EXT_TSTAMP_IMPLEMENTED              _UL_(0)    /* External time stamp */
-#define CAN4_GCLK_ID                             _UL_(50)   /* Index of Generic Clock */
-#define CAN4_MSG_RAM_ADDR                        _UL_(0x20) /* 8-bits MSB */
-#define CAN4_RXD_SYNC_METHOD                     _UL_(0)    /* RXD Synchronization Method */
-#define CAN4_INSTANCE_ID                         _UL_(52)   /* Instance index for CAN4 */
-#define CAN4_MCLK_ID_AHB                         _UL_(60)   /* Index for CAN4 AHB clock */
-#define CAN4_PAC_ID                              _UL_(52)   /* Index for CAN4 registers write protection */
+#define CAN4_DMAC_ID_DEBUG                       (87)       /* DMA CAN Debug Req */
+#define CAN4_ECC_BERR_IMPLEMENTED                (0)        /* ECC error */
+#define CAN4_EXT_TSTAMP_IMPLEMENTED              (0)        /* External time stamp */
+#define CAN4_GCLK_ID                             (50)       /* Index of Generic Clock */
+#define CAN4_MSG_RAM_ADDR                        (0x20)     /* 8-bits MSB */
+#define CAN4_RXD_SYNC_METHOD                     (0)        /* RXD Synchronization Method */
+#define CAN4_INSTANCE_ID                         (52)       /* Instance index for CAN4 */
+#define CAN4_MCLK_ID_AHB                         (60)       /* Index for CAN4 AHB clock */
+#define CAN4_PAC_ID                              (52)       /* Index for CAN4 registers write protection */
 
 #endif /* _PIC32CZCA90_CAN4_INSTANCE_ */
