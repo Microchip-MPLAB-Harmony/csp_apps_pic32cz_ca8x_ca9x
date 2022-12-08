2331a1e9e316d9479a4364890a010856cfa85653
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2022-12-08 18:48:15 +0530

    [CLOCK] Regenerated demo for clock_clock_config_pic32cz_ca80_curiosity_ultra
    MH3-73382
    
    Signed-off-by: Jignesh Moradiya <Jignesh.Moradiya@microchip.com>

--- apps/clock/clock_config/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/clock/clock_config/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -74,16 +74,16 @@ void PORT_Initialize(void)
 
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_DIR = 0x200000;
-   PORT_REGS->GROUP[1].PORT_OUT = 0x200000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0;
+   PORT_REGS->GROUP[1].PORT_DIR = 0x200000U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x200000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_PINCFG[0] = 0x1;
+   PORT_REGS->GROUP[2].PORT_PINCFG[0] = 0x1U;
 
-   PORT_REGS->GROUP[2].PORT_PMUX[0] = 0xc;
+   PORT_REGS->GROUP[2].PORT_PMUX[0] = 0xcU;
 
    /************************** GROUP 3 Initialization *************************/
 
