d893cd9a3908686439dbb210433f7e2ff96ea10d
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-12-08 18:39:27 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_pic32cz_ca80_curiosity_ultra
    MH3-73382
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
+++ apps/rtc/rtc_alarm/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
@@ -54,7 +54,7 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/sercom/usart/plib_sercom5_usart.h"
-#include "peripheral/rtc/plib_rtc.h"
+#include "peripheral/adc/plib_adc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
