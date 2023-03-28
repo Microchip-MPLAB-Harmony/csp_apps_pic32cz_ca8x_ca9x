defb4a232959af089c8e02830db51dc0d8c447e5
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-03-28 11:37:10 +0530

    [PM] Remove print for Off sleep mode in main file of PM_Wakeup_Rtc demo
    MH3-75212

--- apps/pm/pm_wakeup_rtc/firmware/src/main.c
+++ apps/pm/pm_wakeup_rtc/firmware/src/main.c
@@ -166,7 +166,6 @@ int main ( void )
             }
             case OFF_SLEEP_MODE:
             {
-                printf("\n\n\rConfiguring RTC Compare Match for wake up.......");
                 configure_alarm();
                 SYSTICK_TimerStop();
                 printf("\n\rEntering OFF Mode");
