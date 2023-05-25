312e4a1e7ec79eddd8374573ee78ec351321c513
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-25 16:22:24 +0530

    [DMA] Regenerated demo for dma_dma_usart_echo_pic32cz_ca80_curiosity_ultra
    MH3-77689

--- apps/dma/dma_usart_echo/firmware/src/packs/PIC32CZ8110CA80208_DFP/component/fcw.h
+++ apps/dma/dma_usart_echo/firmware/src/packs/PIC32CZ8110CA80208_DFP/component/fcw.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2023-04-05T13:14:57Z */
+/* file generated from device description version 2023-05-18T19:14:05Z */
 #ifndef _PIC32CZCA80_FCW_COMPONENT_H_
 #define _PIC32CZCA80_FCW_COMPONENT_H_
 
