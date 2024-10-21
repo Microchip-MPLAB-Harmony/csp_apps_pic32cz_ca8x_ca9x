ff901fe34ed4030b30ff54dd7d54548069a56e08
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-10-21 17:01:02 +0530

    [DMA] Regenerated demo for dma__dma_crc32_generate__firmware__pic32cz_ca80_curiosity_ultra.X
    MH3-100292

--- apps/dma/dma_crc32_generate/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/dma/dma_crc32_generate/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -81,7 +81,8 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_PINCFG[4] = 0x1U;
+   PORT_REGS->GROUP[2].PORT_DIR = 0x10U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[4] = 0x3U;
    PORT_REGS->GROUP[2].PORT_PINCFG[7] = 0x1U;
 
    PORT_REGS->GROUP[2].PORT_PMUX[2] = 0x3U;
@@ -268,6 +269,14 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
 void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 {
     ((port_group_registers_t*)group)->PORT_DIRCLR = mask;
+    
+    for(uint32_t i = 0U; i < 32U; i++)
+    {
+        if((mask & ((uint32_t)1U << i)) != 0U)
+        {
+            ((port_group_registers_t*)group)->PORT_PINCFG[i] |= PORT_PINCFG_INEN_Msk;
+        }
+    }
 }
 
 // *****************************************************************************
