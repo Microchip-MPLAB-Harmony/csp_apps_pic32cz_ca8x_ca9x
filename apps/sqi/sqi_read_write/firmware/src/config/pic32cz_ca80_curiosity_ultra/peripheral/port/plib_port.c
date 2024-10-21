84629b1c460b3dfc0c15a4fb7a383f95ca6ab2da
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-10-21 16:44:10 +0530

    [SQI] Regenerated demo for sqi__sqi_read_write__firmware__pic32cz_ca80_curiosity_ultra.X
    MH3-100350

--- apps/sqi/sqi_read_write/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/sqi/sqi_read_write/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -71,14 +71,18 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
+   PORT_REGS->GROUP[0].PORT_PINCFG[26] = 0x0U;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[13] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x200000U;
-   PORT_REGS->GROUP[1].PORT_OUT = 0x200000U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x1200000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[0] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0U;
-   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x2U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x6U;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x0U;
 
@@ -102,9 +106,11 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[6].PORT_PINCFG[1] = 0x1U;
    PORT_REGS->GROUP[6].PORT_PINCFG[2] = 0x1U;
    PORT_REGS->GROUP[6].PORT_PINCFG[3] = 0x1U;
+   PORT_REGS->GROUP[6].PORT_PINCFG[7] = 0x0U;
 
    PORT_REGS->GROUP[6].PORT_PMUX[0] = 0x77U;
    PORT_REGS->GROUP[6].PORT_PMUX[1] = 0x77U;
+   PORT_REGS->GROUP[6].PORT_PMUX[3] = 0x0U;
 
 
 }
@@ -275,6 +281,14 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
 void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 {
     ((port_group_registers_t*)group)->PORT_DIRCLR = mask;
+    
+    for(uint32_t i = 0U; i < 32U; i++)
+    {
+        if((mask & ((uint32_t)1U << i)) != 0U)
+        {
+            ((port_group_registers_t*)group)->PORT_PINCFG[i] |= PORT_PINCFG_INEN_Msk;
+        }
+    }
 }
 
 // *****************************************************************************
